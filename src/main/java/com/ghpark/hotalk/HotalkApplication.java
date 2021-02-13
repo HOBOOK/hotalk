1905c87a2dc4957349118a4ee263f7f2f24803cc
ghpark <pkh879@gmail.com>
2021-02-13 16:20:52 +0900

    Create git repository

---
+++ src/main/java/com/ghpark/hotalk/HotalkApplication.java
@@ -0,0 +1,13 @@
+package com.ghpark.hotalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ghpark/hotalk/HotalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghpark.hotalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
